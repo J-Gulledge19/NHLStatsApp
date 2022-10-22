f1931d91485f369ca5ee950c2bd041e5dd3c8552
Josh Gulledge <jgulledge19@gmail.com>
2022-10-22 16:05:59 -0600

    removed bullet points and changed key names

--- app.js
+++ app.js
@@ -2,12 +2,40 @@
 const baseURL = 'https://statsapi.web.nhl.com/';
 
 const key = 'api/v1/teams/'
+
 const statsModifier = '?expand=team.stats'
     
 const url = `${baseURL}${key}`;
 
 const readableKeys = {
     gamesPlayed: 'Games Played',
+    wins: 'Wins',
+    losses: 'Losses',
+    ot: 'Overtimes',
+    pts: 'Points',
+    ptPctg: 'Point Percentage',
+    goalsPerGame: 'Goals Per Game',
+    goalsAgainstPerGame: 'Goals Against Per Game',
+    evGGARatio: 'Goals Against Average',
+    powerPlayPercentage: 'PowerPlay Percentage',
+    powerPlayGoals: 'PowerPlay Goals',
+    powerPlayGoalsAgainst: 'PowerPlay Goals Against',
+    powerPlayOpportunities: 'PowerPlay Opportunities',
+    penaltyKillPercentage: 'Penalty Kill Percentage',
+    shotsPerGame: 'Shots Per Game',
+    shotsAllowed: 'Shots Allowed',
+    winScoreFirst: 'Wins if Scored First',
+    winOppScoreFirst: 'Wins if Opponent Scores First',
+    winLeadFirstPer: 'Wins if Leading in First Period',
+    winLeadSecondPer: 'Win if Leading Second Period',
+    winOutshootOpp: 'Win if Out Shooting Opponent',
+    winOutshotByOpp: 'Win if Out Shot by Opponent',
+    faceOffsTaken: 'Faceoffs Taken',
+    faceOffsWon: 'Faceoff Wins',
+    faceOffsLost: 'Faceoff Losses',
+    faceOffWinPercentage: 'Faceoff Win Percentage',
+    shootingPctg: 'Shooting Percentage',
+    savePctg: 'Save Percentage'
 }
     
 $.ajax(url).then((data) => {
@@ -39,9 +67,9 @@ function renderStats(link){
     console.log(Object.entries(statArea))
     for(const [key, value] of Object.entries(statArea)){
         if (readableKeys[key]){
-            teamStats.push(`<div>${readableKeys[key]}:${value}</div>`)
+            teamStats.push(`<div>${readableKeys[key]}: ${value}</div>`)
         }else{
-            teamStats.push(`<div>${key}:${value}</div>`)
+            teamStats.push(`<div>${key}: ${value}</div>`)
         }
     }
       $('main').html(`${teamStats}`);
